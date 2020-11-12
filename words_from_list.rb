8e890054fade315bd8de2bf751e42c4532d2d090
antoniotto71 <antoniotto71@yahoo.it>
2020-11-12 15:46:04 +0100

    Fixed Rubocop Maybe

--- words_from_list.rb
+++ words_from_list.rb
@@ -2,5 +2,9 @@ def words_from_list(string)
   string.downcase.scan(/[\w']+/)
 end
 
-p words_from_list("Let’s round off this section with a simple program that calculates the number of times each
-  word occurs in some text. (So, for example, in this sentence, the word the occurs two times.)")
+text = "Let’s round off this section with a simple program that calculates the number of times each
+word occurs in some text. (So, for example, in this sentence, the word the occurs two times.)"
+
+p words_from_list(text)
+
+puts words_from_list(text).class
