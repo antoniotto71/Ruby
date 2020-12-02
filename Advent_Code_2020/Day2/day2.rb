a64cff0e7ea8653b5d24a5a99f4bccb1b8b4e326
antoniotto71 <antoniotto71@yahoo.it>
2020-12-02 17:05:53 +0100

    Day 2 Commit

--- Advent_Code_2020/Day2/day2.rb
+++ Advent_Code_2020/Day2/day2.rb
@@ -8,7 +8,12 @@ str2 = str.map { |x, y| x + y }
 p str
 p str2
 
-def decode(key, value)
-
-
+def decode(key)
+  nums = key.scan(/(\d+)/)
+  letter = key.scan(/[a-z]/)
+  nums + letter
 end
+
+
+p decode("1-3 c")
+
