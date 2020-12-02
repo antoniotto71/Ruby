9ca423e348e7df770424506f976185f812253afa
antoniotto71 <antoniotto71@yahoo.it>
2020-12-02 22:12:32 +0100

    Day2 2

--- Advent_Code_2020/Day2/day2.rb
+++ Advent_Code_2020/Day2/day2.rb
@@ -1,19 +1,36 @@
+require 'debase'
 
 array = File.readlines('input.txt')
 
 str = array.map { |x| x.strip.split(':') }
 
-str2 = str.map { |x, y| x + y }
-
-p str
-p str2
-
-def decode(key)
-  nums = key.scan(/(\d+)/)
-  letter = key.scan(/[a-z]/)
-  nums + letter
+def decode(key, password)
+  nums = key.scan(/\d+/)
+  letter = key.scan(/[a-z]/).to_s
+  inf = nums[0].to_i
+  sup = nums[1].to_i
+  digit = password.count letter
+  (digit >= inf) && (digit <= sup)
 end
 
+def decode2(key, password)
+  nums = key.scan(/\d+/)
+  letter = key.scan(/[a-z]/)[0]
+  first = nums[0].to_i
+  second = nums[1].to_i
+  m = password[first] == letter
+  n = password[second] == letter
+  m ^ n
+end
 
-p decode("1-3 c")
+#str2 = str.map { |x, y| decode(x, y) }
+
+str3 = str.map { |x, y| decode2(x, y) }
+
+
+p decode2("1-3 a", "abcde")
+
+#p str2.count(true)
+
+p str3.count(true)
 
