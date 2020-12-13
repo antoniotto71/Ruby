5d0d4f05e0baa510db7a24bd58f3ca54aa88fc15
antoniotto71 <antoniotto71@yahoo.it>
2020-12-13 20:33:54 +0100

    Day 13 first one

--- Advent_Code_2020/Day13/day13.rb
+++ Advent_Code_2020/Day13/day13.rb
@@ -1,4 +1,4 @@
-data = File.readlines("input.txt", "\n").map(&:chop).to_a
+data = File.readlines("input_test.txt", "\n").map.to_a
 
 data2 = data.map {|x| x.gsub(",x", "").split(",").map(&:to_i)}
 
@@ -17,3 +17,57 @@ def transform(arr)
 end
 
 p transform(data2)
+
+data3 = data.map {|x| x.gsub("x", "0").split(",").map(&:to_i)}
+
+p data3
+
+p data3[1]
+
+data_second = data3[1].each_with_index.
+  map { |x,y| [x,y] }.
+  each.reject { |x, y| x == 0 }
+
+p data_second
+
+@target = data_second.map { |x,y| x-y }
+
+@target[0] = 0
+
+@divs = data_second.map {|x,y| x}
+
+p @divs
+
+p @target
+
+@target2 = [0, 32, 804, 19, 15, 10, 393, 18, 2]
+
+def find_result
+  test2 = []
+  counter = 0
+  (1068781..1068782).each do |num|
+    test = []
+    @divs.each do |elem|
+      test << num%elem
+    end
+    test2 << test
+    counter += 1
+  end
+  return test2
+end
+
+def find_result2
+  test2 = []
+  counter = 0
+  (1..).each do |num|
+    test = []
+    @divs.each do |elem|
+      test << num%elem
+    end
+    counter += 1
+    break if test == @target
+  end
+  counter
+end
+
+p find_result2
