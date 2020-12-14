1795ead3e3484d3d4aa3d46408756019603b3369
antoniotto71 <antoniotto71@yahoo.it>
2020-12-14 20:00:50 +0100

    Day 10 modified

--- Advent_Code_2020/Day10/day10.rb
+++ Advent_Code_2020/Day10/day10.rb
@@ -2,10 +2,13 @@ arr = File.readlines("input.txt", "\n").map(&:chomp).map(&:to_i).sort
 
 p arr
 
-# diff_1 = arr.chunk_while {|i, j| i + 1 == j }
+arr2 = [0, *arr, arr.max + 3 ]
 
-diff_3 = arr.each_with_object([]) {|i, j| j-i == 3}
+p arr2
 
+arr3 = arr2.each_cons(2).map { |x, y| y - x  }
 
+result = arr3.count(1) * arr3.count(3)
 
-p diff_3
+p result
+
