c9b0dfda31bafb7aa89a75edb17df252b91d710c
antoniotto71 <antoniotto71@yahoo.it>
2020-12-22 22:34:26 +0100

    Still day 15

--- Advent_Code_2020/Day15/day15.rb
+++ Advent_Code_2020/Day15/day15.rb
@@ -1,11 +1,13 @@
-data = "0,3,6"
+data = "19,20,14,0,9,1"
 input = data.split(",").map(&:to_i)
 p input
 
-def solution(arr, target)
+def solution(arr, turn)
+  target = turn - 2
   (arr.size - 1).upto(target).each do |number|
+    int = arr[0, number]
     if arr[0,number -1].include?(arr[number])
-      int = arr[0, number -1].rindex(arr[number])
+      int = arr[0, number].rindex(arr[number])
       arr << (number - int)
     else
       arr << 0
@@ -14,4 +16,4 @@ def solution(arr, target)
   return arr
 end
 
-puts solution(input, 9)
+puts solution(input, 2020).last
---
+++ Advent_Code_2020/Day15/day15_ref.rb
@@ -0,0 +1,27 @@
+
+data = "19,20,14,0,9,1"
+input = data.split(",").map(&:to_i)
+
+p input
+
+def find_result(input, target)
+  hashtable = Hash.new(0)
+  start_arr = input[0..-2]
+  last = input.last
+  start_arr.each do |item|
+    hashtable[item] = start_arr.index(item) + 1
+  end
+  ((input.size + 1)..target).each do |turn|
+    if hashtable.has_key?(last)
+      following = turn - hashtable[last]
+    else following = 0
+    end
+    hashtable[last] = turn - 1
+    hashtable[following] = turn
+    last = following
+  end
+  last
+end
+
+
+p find_result(input, 2020)
